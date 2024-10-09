8c60233b8cdf2f261740c1a982ccf6cd9ef04f18
Stefano Pigozzi <me@steffo.eu>
2024-10-10 01:32:49 +0200

    Where the heck did that "out" come from

--- out/production/peafowl/mass-effect-3.md
+++
@@ -1,9 +0,0 @@
----
-name: Mass Effect 3
-active: false
-rating: 78
-progress: beaten
-hours_played: 0
-identifiers: []
----
-
--- out/production/peafowl/scripting/installSortOnLoad.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    const tableId = document.querySelector("table").id
-    installSort(tableId)
-    sortTable(tableId, readRating, (a, b) => b - a)
-}
